c83631181e57c8608b2b2a8cecadad490893a7d7
Nick Pallas <nickpallas29@gmail.com>
2024-05-23 14:45:47 -0700

    Pushed While server was on

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // eslint-disable-next-line no-undef
 module.exports = {
-    API_URL: "https://cnt-e6c3d047-660a-4ca3-a3b4-1defb859da1d.containerhub.tripleten-services.com"
+    API_URL: "https://cnt-02164f2a-327a-411e-bdc1-a026a691abfa.containerhub.tripleten-services.com"
 }
